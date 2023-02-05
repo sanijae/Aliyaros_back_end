10b59b4d8ad2d8fc2ae2e02c7238d46fb51deeb2
muhammad sani <muhammadsanijae@gmail.com>
2023-02-05 07:16:21 +0100

    routes commmit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='aliyaros_back_end' >
+    <BrowserRouter basename='/aliyaros_back_end' >
       <App />
     </BrowserRouter>
   </React.StrictMode>
