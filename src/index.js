eb61bd3b573a4f351895337d130ab45b6dc5067d
muhammad sani <muhammadsanijae@gmail.com>
2023-02-05 12:31:32 +0100

    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/aliyaros_back_end' >
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 // basename={'/aliyaros_back_end/'}
+//basename={process.env.PUBLIC_URL}
