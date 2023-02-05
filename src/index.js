ac31939df7828091a6c8e8109a99c87d4eb96e4e
muhammad sani <muhammadsanijae@gmail.com>
2023-02-05 06:36:19 +0100

    redirect commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter  basename={'/aliyaros_back_end'}>
+    <BrowserRouter  basename={'/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
