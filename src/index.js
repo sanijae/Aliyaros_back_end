4aed8a5344abbf62d2e00eddf9e5f7c25da94c4c
muhammad sani <muhammadsanijae@gmail.com>
2023-02-05 07:04:36 +0100

    routes commmit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter >
+    <BrowserRouter basename='aliyaros_back_end' >
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 // basename={'/aliyaros_back_end/'}
